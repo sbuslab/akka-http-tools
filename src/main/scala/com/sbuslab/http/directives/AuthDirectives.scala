e1c8f8693da580100993cc8ae600bab0705b68e8
kulikov <kulikov.dm@gmail.com>
2019-08-15 17:30:21 +0300

    add implicit sbus Context to auth directive

--- src/main/scala/com/sbuslab/http/directives/AuthDirectives.scala
+++ src/main/scala/com/sbuslab/http/directives/AuthDirectives.scala
@@ -5,18 +5,20 @@ import scala.concurrent.Future
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.server.{Directives, Route}
 
+import com.sbuslab.sbus.Context
+
 
 trait AuthProvider[T] {
-  def auth(request: HttpRequest): Future[T]
+  def auth(request: HttpRequest)(implicit context: Context): Future[T]
 }
 
 
 trait AuthDirectives extends Directives {
 
-  protected def auth[T](f: ⇒ Route)(implicit authProvider: AuthProvider[T]): Route =
+  protected def auth[T](f: ⇒ Route)(implicit authProvider: AuthProvider[T], context: Context = Context.empty): Route =
     auth { _: T ⇒ f }
 
-  protected def auth[T](inner: T ⇒ Route)(implicit authProvider: AuthProvider[T]): Route =
+  protected def auth[T](inner: T ⇒ Route)(implicit authProvider: AuthProvider[T], context: Context = Context.empty): Route =
     extractRequest { req ⇒
       onSuccess(authProvider.auth(req))(inner)
     }
